c14aec14ec50b2cf8b95ad69856d466919d81076
Ajay Varma Nandre <188r1a0442@gmail.com>
2026-07-20 13:22:14 +0530

    Add new template

--- src/main/java/org/springframework/samples/petclinic/developer/DeveloperUiController.java
+++ src/main/java/org/springframework/samples/petclinic/developer/DeveloperUiController.java
@@ -4,6 +4,9 @@ import org.springframework.samples.petclinic.developer.service.DeveloperService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.samples.petclinic.developer.entity.Developer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class DeveloperUiController {
@@ -20,6 +23,24 @@ public class DeveloperUiController {
 		model.addAttribute("developers", this.developerService.findAll());
 
 		return "developers/list";
+	}
+
+	@GetMapping("/developers/edit/{id}")
+	public String showEditForm(@PathVariable Long id, Model model) {
+
+		Developer developer = this.developerService.findById(id);
+
+		model.addAttribute("developer", developer);
+
+		return "developers/edit";
+	}
+
+	@PostMapping("/developers/update/{id}")
+	public String updateDeveloper(@PathVariable Long id, Developer developer) {
+
+		this.developerService.updateDeveloper(id, developer);
+
+		return "redirect:/developers";
 	}
 
 }

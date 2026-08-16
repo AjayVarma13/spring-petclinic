f92f6e0a2b889237e78263c644875bd123658351
Ajay Varma Nandre <188r1a0442@gmail.com>
2026-08-16 23:06:14 +0530

    add in memory

---
+++ src/main/java/org/springframework/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.springframework.samples.petclinic.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	InMemoryUserDetailsManager userDetailsService() {
+
+	}
+
+}

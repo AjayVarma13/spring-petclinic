a86b89a8e4ee2bcaa2e240fa440742b36699f429
Ajay Varma Nandre <188r1a0442@gmail.com>
2026-08-19 21:53:51 +0530

    update security config

--- src/main/java/org/springframework/config/SecurityConfig.java
+++ src/main/java/org/springframework/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
 			.password("ajay123")
 			.roles("USER")
             .build();
-             return InMemoryUserDetailsManager(user);
+             return new InMemoryUserDetailsManager();
 	}
 
 }

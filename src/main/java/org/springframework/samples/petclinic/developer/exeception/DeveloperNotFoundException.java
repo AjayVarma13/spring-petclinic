d89be6bf9caebcc631804340dda3c16d653f381f
Ajay Varma Nandre <188r1a0442@gmail.com>
2026-08-26 23:43:04 +0530

    update message parameter for exception

--- src/main/java/org/springframework/samples/petclinic/developer/exeception/DeveloperNotFoundException.java
+++ src/main/java/org/springframework/samples/petclinic/developer/exeception/DeveloperNotFoundException.java
@@ -2,8 +2,8 @@ package org.springframework.samples.petclinic.developer.exception;
 
 public class DeveloperNotFoundException extends RuntimeException {
 
-	public DeveloperNotFoundException(String msg) {
-		super(msg);
+	public DeveloperNotFoundException(String message) {
+		super(message);
 	}
 
 }

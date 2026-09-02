419439253c325f154dc34605d5d4c196b8aab2a5
Ajay Varma Nandre <188r1a0442@gmail.com>
2026-09-02 22:48:01 +0530

    use save method springboot

--- src/main/java/org/springframework/samples/petclinic/developer/service/DeveloperService.java
+++ src/main/java/org/springframework/samples/petclinic/developer/service/DeveloperService.java
@@ -17,7 +17,7 @@ public class DeveloperService {
 	}
 
 	public Developer save(Developer developer) {
-		return this.developerRepository.saveAll(developer);
+		return this.developerRepository.save(developer);
 	}
 
 	public List<Developer> findAll() {

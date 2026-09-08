9eaa7442c829cd1a2600004099b20f9b742554f8
Ajay Varma Nandre <188r1a0442@gmail.com>
2026-09-08 22:40:44 +0530

    delete the identity

--- src/main/java/org/springframework/samples/petclinic/developer/service/DeveloperService.java
+++ src/main/java/org/springframework/samples/petclinic/developer/service/DeveloperService.java
@@ -31,7 +31,7 @@ public class DeveloperService {
 	}
 
 	public void deleteById(Long id) {
-		this.developerRepository.deleteById(id);
+		this.developerRepository.delete(id);
 	}
 
 	public Developer updateDeveloper(Long id, Developer developer) {

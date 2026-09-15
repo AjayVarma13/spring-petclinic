1f2b523fa2fd4e8dae4d9cad649db7af8c82a84b
Ajay Varma Nandre <188r1a0442@gmail.com>
2026-09-15 13:12:26 +0530

    springboot developerUiController

--- src/main/java/org/springframework/samples/petclinic/developer/DeveloperUiController.java
+++ src/main/java/org/springframework/samples/petclinic/developer/DeveloperUiController.java
@@ -11,7 +11,7 @@ import jakarta.validation.Valid;
 import org.springframework.validation.BindingResult;
 
 @Controller
-public class DeveloperUiControllers {
+public class DeveloperUiController {
 
 	private final DeveloperService developerService;
 
